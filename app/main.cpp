6f8278f4ce61ffc28548736bac916669ea6ca249
timur <timurkashapov15@gmail.com>
2025-02-22 11:20:16 +0300

    glog and dlib was added

--- app/main.cpp
+++ app/main.cpp
@@ -1,41 +1,11 @@
 #include <iostream>
 #include <cstdint>
-#include "bitset.hpp"
-#include "bitmask.hpp"
-#include "optional.hpp"
-#include "bimap.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <dlib/image_processing/frontal_face_detector.h>
 
-enum PowerMode : unsigned char
-{
-    None,
-    On,
-    Off,
-    MaxValue,
-};
-
-enum class Color : unsigned char
-{
-    Red,
-    Green,
-    Blue,
-    MaxValue,
-};
-
-enum Suit : int
-{ 
-    Diamonds, 
-    Hearts, 
-    Clubs, 
-    Spades
-};
 
 int main()
 {
-    BitMask<PowerMode, PowerMode::MaxValue> pwrMode;
-    BitMask<Color, static_cast<unsigned char>(Color::MaxValue)> color;
-    // BitMask<Suit, 8> suit;
-    BitSet<uint8_t, 8> mask;
-    optional<int> opt;
-    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
