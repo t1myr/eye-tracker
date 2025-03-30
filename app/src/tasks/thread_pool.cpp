1914f72b2681b48b7e9d9f458a9c1bf9098ae989
Timur <timurkashapov15@gmail.com>
2025-03-30 21:01:06 +0300

    Add thread pool class

---
+++ app/src/tasks/thread_pool.cpp
@@ -0,0 +1,74 @@
+#include "thread_pool.hpp"
+
+#include <cassert>
+
+
+std::unique_ptr<ThreadPool> ThreadPool::m_instance = nullptr;
+
+/**
+ * @brief Инициализация пула
+ * @param numThreads 
+ */
+void ThreadPool::init(std::size_t numThreads)
+{
+
+    if(!m_instance)
+        m_instance = std::make_unique<ThreadPool>(numThreads);
+}
+
+/**
+ * @brief Получаем экземпляр пула
+ * @return ThreadPool* 
+ */
+ThreadPool* ThreadPool::get()
+{
+    assert(m_instance != nullptr && "Instance must be initialized before");
+    return m_instance.get();
+}
+    
+/**
+ * @brief Выдача количества свободных исполнителей
+ * @return std::size_t 
+ */
+size_t ThreadPool::getFreeWorkersNum() {
+    std::lock_guard<std::mutex> lock(m_queueMutex);
+    return m_workers.size() - m_busyWorkers;
+}
+
+/**
+ * @brief Конструктор
+ * @param numThreads 
+ */
+ThreadPool::ThreadPool(size_t numThreads) : m_stop(false), m_busyWorkers(0) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        m_workers.emplace_back([this] {
+            while (true) {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(m_queueMutex);
+                    m_condition.wait(lock, [this] { return m_stop || !m_tasks.empty(); });
+                    if (m_stop && m_tasks.empty()) return;
+                    task = std::move(m_tasks.front());
+                    m_tasks.pop();
+                    ++m_busyWorkers;
+                }
+                task();
+                {
+                    std::lock_guard<std::mutex> lock(m_queueMutex);
+                    --m_busyWorkers;
+                }
+                m_condition.notify_all();
+            }
+        });
+    }
+}
+
+/**
+ * @brief Деструктор
+ */
+ThreadPool::~ThreadPool() {
+    m_stop = true;
+    m_condition.notify_all();
+    for (std::thread& worker : m_workers)
+        worker.join();
+}
---
+++ app/src/tasks/thread_pool.hpp
@@ -0,0 +1,145 @@
+#ifndef _THREAD_POOL_HPP_
+#define _THREAD_POOL_HPP_
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <future>
+#include <functional>
+#include <atomic>
+#include <condition_variable>
+
+/**
+ * @brief Пул потоков
+ */
+class ThreadPool {
+public:
+
+    // Запрещаем копирование и перемещение
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool(ThreadPool&&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool& operator=(ThreadPool&&) = delete;
+    
+    /**
+     * @brief Инициализация пула
+     * @param numThreads 
+     */
+    static void init(std::size_t numThreads);
+
+    /**
+     * @brief Получаем экземпляр пула
+     * @return ThreadPool* 
+     */
+    static ThreadPool* get();
+
+    /**
+     * @brief Добавляем задачу в очередь
+     * @tparam F 
+     * @tparam Args
+     * @param f callable объект, функция будет вызвана в отдельном потоке
+     * @param args аргументы
+     * @return std::future<std::invoke_result_t<F, Args...>> future
+     */
+    template<typename F, typename... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>;
+
+    /**
+     * @brief Ставим набор в задач в очередь на синхронное исполнение
+     * @tparam F 
+     * @tparam Args 
+     * @param m_tasks задачи
+     * @return std::vector<std::future<void>> 
+     */
+    template<typename F, typename... Args>
+    auto ThreadPool::enqueueAcquire(std::vector<std::function<void()>> tasks) -> std::vector<std::future<void>>;
+
+    /**
+     * @brief Выдача количества свободных исполнителей
+     * @return std::size_t 
+     */
+    std::size_t getFreeWorkersNum();
+
+private:
+    std::vector<std::thread> m_workers;
+    std::queue<std::function<void()>> m_tasks;
+
+    std::mutex m_queueMutex;
+    std::condition_variable m_condition;
+    std::atomic<bool> m_stop;
+    std::size_t m_busyWorkers;
+
+    static std::unique_ptr<ThreadPool> m_instance;
+
+    /**
+     * @brief Конструктор
+     * @param numThreads Количество потоков в пуле
+     */
+    ThreadPool(std::size_t numThreads);
+
+    /**
+     * @brief Деструктор
+     */
+    ~ThreadPool();
+};
+    
+template<typename F, typename... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> {
+    using return_type = std::invoke_result_t<F, Args...>;
+
+    //Создаем задачу
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    //Добавляем задачу в очередь
+    std::future<return_type> res = task->get_future();
+    {
+        std::lock_guard<std::mutex> lock(m_queueMutex);
+        m_tasks.emplace([task]() { (*task)(); });
+    }
+    //Оповещаем об этом
+    m_condition.notify_one();
+    return res;
+}
+    
+template<typename F, typename... Args>
+auto ThreadPool::enqueueAcquire(std::vector<std::function<void()>> tasks) -> std::vector<std::future<void>> 
+{
+    std::vector<std::future<void>> futures;
+    {
+        auto numThreads = tasks.size();
+
+        std::unique_lock<std::mutex> lock(m_queueMutex);
+
+        // Ждём, пока освободится нужное количество потоков
+        m_condition.wait(lock, [this, numThreads] {
+            return m_workers.size() - m_busyWorkers >= tasks.size();
+        });
+
+        // "Бронируем" потоки
+        m_busyWorkers += tasks.size();
+
+        // Добавляем задачи в очередь
+        for (auto& task : tasks) {
+            auto packagedTask = std::make_shared<std::packaged_task<void()>>(task);
+            futures.push_back(packagedTask->get_future());
+
+            this->m_tasks.emplace([this, packagedTask, numThreads]() {
+                (*packagedTask)();
+                
+                // Освобождаем потоки после выполнения
+                {
+                    std::lock_guard<std::mutex> lock(m_queueMutex);
+                    m_busyWorkers--;
+                }
+                m_condition.notify_all();
+            });
+        }
+    }
+
+    m_condition.notify_all();
+    return futures;
+}
+
+#endif //_THREAD_POOL_HPP_
