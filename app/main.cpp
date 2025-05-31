75c9410262408c57f40967c1846484bb26979e52
Timur <timurkashapov15@gmail.com>
2025-05-31 13:30:01 +0300

    Argparse was added

--- app/main.cpp
+++ app/main.cpp
@@ -10,22 +10,54 @@
 #include "mouse_control/mouse_control_task.hpp"
 #include "tasks/thread_pool.hpp"
 
-
-using namespace cv;
-using namespace std;
+//argparse
+#include "argparse/argparse.hpp"
 
 static constexpr std::size_t kMaxThreadPoolSize = 8;
 // "C:\\Users\\timur\\Projects\\eye-tracker\\data\\calibrate"
 
 int main(int argc, char* argv[])
 {
+    //Инициализируем парсер аргументов
+    argparse::ArgumentParser program("eye_tracker");
+
+    //Аргумент логгирования
+    program.add_argument("-l", "--logging")
+    .help("Logging level")
+    .scan<'i', int>()
+    .default_value(static_cast<int>(spdlog::level::info));
+
+    //Аргумент конфиг
+    program.add_argument("-p", "--predictor")
+    .help("Path to shape predictor")
+    .required();
+
+    //Парсим аргументы
+    try {
+       program.parse_args(argc, argv);   
+    }
+    catch (const std::exception& err) {
+        std::cout << "Cannot parse some argument" << std::endl;
+        std::cout << program << std::endl;
+        std::exit(1);
+    }
+
+    auto logLevel = program.get<int>("-l");
+    auto predictorPath = program.get<std::string>("--predictor");
+
+    if(logLevel < spdlog::level::trace || logLevel >= spdlog::level::off)
+    {
+        std::cout << program << std::endl;
+        std::exit(1);
+    }
+
     //Инициализация логгирования
-    Logger::init(spdlog::level::info);
+    Logger::init(static_cast<spdlog::level::level_enum>(logLevel));
     Logger::set_thread_name("main");
     ThreadPool::init(kMaxThreadPoolSize);
     spdlog::info("Starting the program");
     //Инициализируем задачи
-    ControlTask ctrl("C:\\Users\\timur\\Projects\\eye-tracker\\data\\shape_predictor_68_face_landmarks.dat");
+    ControlTask ctrl(predictorPath);
     VirtualScene scene;
     InputControlTask input;
     MouseControlTask mouse(MouseControlTask::Mode::kVisual);
